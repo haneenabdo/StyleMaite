5fbd772e18276dda25bf6629de870dcbdebe7380
Haneen Abdo <haneenabdo04@gmail.com>
2025-01-09 00:46:31 -0500

    updated API url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: 'https://stylemaite-api.onrender.com',
 });
 
 export default API;
